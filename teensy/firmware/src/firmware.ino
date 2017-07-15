6fe3cc762901bc376701fb6d391789f2989d730c
Juan Jimeno <jimenojmm@gmail.com>
2017-07-15 12:36:18 +0800

    cleaned up

--- teensy/firmware/src/firmware.ino
+++ teensy/firmware/src/firmware.ino
@@ -1,31 +1,3 @@
-/*
- Copyright (c) 2016, Juan Jimeno
- All rights reserved.
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice,
- this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- * Neither the name of  nor the names of its contributors may be used to
- endorse or promote products derived from this software without specific
- prior written permission.
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
-
- NOTES: All Odd Number Motors are on the left side of the robot
-        All Even Number Motors are on the right side of the robot
- */
 #if (ARDUINO >= 100)
 #include <Arduino.h>
 #else
@@ -62,10 +34,10 @@
 
 #define ENCODER_OPTIMIZE_INTERRUPTS
 
-Encoder motor1_encoder(MOTOR1_ENCODER_A,MOTOR1_ENCODER_B);
-Encoder motor2_encoder(MOTOR2_ENCODER_A,MOTOR2_ENCODER_B); 
-Encoder motor3_encoder(MOTOR3_ENCODER_A,MOTOR3_ENCODER_B); 
-Encoder motor4_encoder(MOTOR4_ENCODER_A,MOTOR4_ENCODER_B); 
+Encoder motor1_encoder(MOTOR1_ENCODER_A, MOTOR1_ENCODER_B);
+Encoder motor2_encoder(MOTOR2_ENCODER_A, MOTOR2_ENCODER_B); 
+Encoder motor3_encoder(MOTOR3_ENCODER_A, MOTOR3_ENCODER_B); 
+Encoder motor4_encoder(MOTOR4_ENCODER_A, MOTOR4_ENCODER_B); 
 
 Servo steering_servo;
 
