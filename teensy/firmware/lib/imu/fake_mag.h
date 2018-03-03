192a27472d13b58c0d2d15a7dba29493d5621702
Juan Jimeno <jimenojmm@gmail.com>
2018-03-03 18:07:05 +0800

    added fake_mag to normalize common functions

---
+++ teensy/firmware/lib/imu/fake_mag.h
@@ -0,0 +1,32 @@
+#ifndef _FAKE_MAG_H_
+#define _FAKE_MAG_H_
+
+
+//This a place holder class for devices that does not have a magnetometer
+
+class FakeMag
+{
+    public:
+        FakeMag();
+        bool initialize();
+        void getHeading(int16_t* mx, int16_t* my, int16_t* mz);
+};
+
+FakeMag::FakeMag()
+{
+
+}
+
+bool  FakeMag::initialize()
+{
+    return true;
+}
+
+void FakeMag::getHeading(int16_t* mx, int16_t* my, int16_t* mz)
+{
+    *mx = 0;
+    *my = 0;
+    *mz = 0;
+}
+
+#endif
