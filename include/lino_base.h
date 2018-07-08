b7fb038e41b68e6e1c6251262bdc0ccab2e0cb4b
Juan Jimeno <jimenojmm@gmail.com>
2018-07-08 14:41:51 +0800

    added odometry class as part of refactoring odometry node to solely use encoders

---
+++ include/lino_base.h
@@ -0,0 +1,31 @@
+#ifndef LINO_BASE_H
+#define LINO_BASE_H
+
+#include <ros/ros.h>
+#include <lino_msgs/Velocities.h>
+#include <tf/transform_broadcaster.h>
+
+class LinoBase
+{
+public:
+    LinoBase();
+    void velCallback(const lino_msgs::Velocities& vel);
+
+
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher odom_publisher_;
+    tf::TransformBroadcaster odom_broadcaster_;
+
+    float steering_angle_;
+    float linear_velocity_x_;
+    float linear_velocity_y_;
+    float angular_velocity_z_;
+    ros::Time last_vel_time_;
+    float vel_dt_;
+    float x_pos_;
+    float y_pos_;
+    float heading_;
+};
+
+#endif
---
+++ src/lino_base.cpp
@@ -0,0 +1,80 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include <lino_base.h>
+
+LinoBase::LinoBase():
+    linear_velocity_x_(0),
+    linear_velocity_y_(0),
+    angular_velocity_z_(0),
+    last_vel_time_(0),
+    vel_dt_(0),
+    x_pos_(0),
+    y_pos_(0),
+    heading_(0)
+{
+    odom_publisher_ = nh_.advertise<nav_msgs::Odometry>("odom", 50);
+}
+
+void LinoBase::velCallback(const lino_msgs::Velocities& vel)
+{
+    ros::Time current_time = ros::Time::now();
+
+    linear_velocity_x_ = vel.linear_x;
+    linear_velocity_y_ = vel.linear_y;
+    angular_velocity_z_ = vel.angular_z;
+
+    vel_dt_ = (current_time - last_vel_time_).toSec();
+    last_vel_time_ = current_time;
+
+    double delta_heading = angular_velocity_z_ * vel_dt_; //radians
+    double delta_x = (linear_velocity_x_ * cos(heading_) - linear_velocity_y_ * sin(heading_)) * vel_dt_; //m
+    double delta_y = (linear_velocity_x_ * sin(heading_) + linear_velocity_y_ * cos(heading_)) * vel_dt_; //m
+
+    //calculate current position of the robot
+    x_pos_ += delta_x;
+    y_pos_ += delta_y;
+    heading_ += delta_heading;
+
+    //calculate robot's heading in quarternion angle
+    //ROS has a function to calculate yaw in quaternion angle
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(heading_);
+
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_footprint";
+    //robot's position in x,y, and z
+    odom_trans.transform.translation.x = x_pos_;
+    odom_trans.transform.translation.y = y_pos_;
+    odom_trans.transform.translation.z = 0.0;
+    //robot's heading in quaternion
+    odom_trans.transform.rotation = odom_quat;
+    odom_trans.header.stamp = current_time;
+    //publish robot's tf using odom_trans object
+    odom_broadcaster_.sendTransform(odom_trans);
+
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
+    //robot's position in x,y, and z
+    odom.pose.pose.position.x = x_pos_;
+    odom.pose.pose.position.y = y_pos_;
+    odom.pose.pose.position.z = 0.0;
+    //robot's heading in quaternion
+    odom.pose.pose.orientation = odom_quat;
+
+    odom.child_frame_id = "base_footprint";
+    //linear speed from encoders
+    odom.twist.twist.linear.x = linear_velocity_x_;
+    odom.twist.twist.linear.y = linear_velocity_y_;
+    odom.twist.twist.linear.z = 0.0;
+
+    odom.twist.twist.angular.x = 0.0;
+    odom.twist.twist.angular.y = 0.0;
+    //angular speed from IMU
+    odom.twist.twist.angular.z = angular_velocity_z_;
+
+    //TODO: include covariance matrix here
+
+    odom_publisher_.publish(odom);
+}
