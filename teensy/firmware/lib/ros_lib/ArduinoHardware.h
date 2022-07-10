62ff847abf48f29c824c23892da06be781dfa178
Juan Jimeno <jimenojmm@gmail.com>
2022-07-10 12:44:06 +0800

    added teensy41

--- teensy/firmware/lib/ros_lib/ArduinoHardware.h
+++ teensy/firmware/lib/ros_lib/ArduinoHardware.h
@@ -41,7 +41,7 @@
   #include <WProgram.h>  // Arduino 0022
 #endif
 
-#if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__MKL26Z64__)
+#if defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__MKL26Z64__) || defined(__IMXRT1062__)
   #if defined(USE_TEENSY_HW_SERIAL)
     #define SERIAL_CLASS HardwareSerial // Teensy HW Serial
   #else
@@ -84,6 +84,10 @@ class ArduinoHardware {
       this->iostream = h.iostream;
       this->baud_ = h.baud_;
     }
+
+    void setPort(SERIAL_CLASS* io){
+      this->iostream = io;
+    }
   
     void setBaud(long baud){
       this->baud_= baud;
@@ -101,8 +105,7 @@ class ArduinoHardware {
 
     int read(){return iostream->read();};
     void write(uint8_t* data, int length){
-      for(int i=0; i<length; i++)
-        iostream->write(data[i]);
+      iostream->write(data, length);
     }
 
     unsigned long time(){return millis();}
